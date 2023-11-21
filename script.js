0661ebdff3033df526fccec83fd538c624da7d01
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-11-21 17:29:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
+
+// Constructor function SportsCar
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model); // calling super constructor
+  this.topSpeed = topSpeed;
+}
+
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
+
 
 // Do not change the code below
 window.Car = Car;
